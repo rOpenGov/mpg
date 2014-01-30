c7fadaad7c5ac85dea7f8301dca9bd9978b37d33
leeper <thosjleeper@gmail.com>
2014-01-30 08:54:38 +0100

    set return value for mympgVehicles

--- R/mympgVehicles.R
+++ R/mympgVehicles.R
@@ -1,8 +1,20 @@
 mympgVehicles <-
-function(make, model, ...){
-    response <- feQuery(paste('vehicles?make=',make,'&model=',model,sep=''),
-                        baseurl='http://www.fueleconomy.gov/ws/rest/ympg/shared/')
-    out <- response
-    class(out) <- c('mympgVehicles',class(out))
+function(make, model, pick=FALSE, graphics=TRUE, ...){
+    if(pick){
+        makes <- mympgMakes()
+        make <- menu(makes, title = "Make", graphics = graphics)
+        models <- mympgModels(make=makes[make])
+        model <- menu(models, title = "Model", graphics = graphics)
+        out <- mympgVehicles(make=makes[make], model=models[model])
+    } else{
+        if(is.null(make))
+            stop("Must supply 'make' | set 'pick=TRUE'")
+        if(is.null(make))
+            stop("Must supply 'model' | set 'pick=TRUE'")
+        response <- feQuery(paste('vehicles?make=',make,'&model=',model,sep=''),
+                            baseurl='http://www.fueleconomy.gov/ws/rest/ympg/shared/')
+        out <- xmlToDataFrame(response)
+    }
+    #class(out) <- c('mympgVehicles',class(out))
     return(out)
 }
