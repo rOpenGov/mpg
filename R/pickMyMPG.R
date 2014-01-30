e04d28654784634e6a7dcbe11c179562a5dbb79b
leeper <thosjleeper@gmail.com>
2014-01-30 09:04:18 +0100

    remove unnecessary pickMyMPG function

--- R/pickMyMPG.R
+++
@@ -1,9 +0,0 @@
-pickMyMPG <-
-function(graphics=TRUE){
-    makes <- mympgMakes()
-    make <- menu(makes, title = "Make", graphics = graphics)
-    models <- mympgModels(make=makes[make])
-    model <- menu(models, title = "Model", graphics = graphics)
-    opts <- mympgVehicles(make=makes[make], model=models[model])
-    return(opts)
-}
